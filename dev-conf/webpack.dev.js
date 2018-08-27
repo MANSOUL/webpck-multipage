6ab750522adb196043511500a5882fa71384ecfb
KuangGuanghu <xiaohu12347@gmail.com>
2018-08-27 13:03:55 +0800

    Update webpack.dev.js

--- dev-conf/webpack.dev.js
+++ dev-conf/webpack.dev.js
@@ -1,7 +1,16 @@
 const path = require('path')
 const merge = require('webpack-merge')
-const {host, port} = require('../config.js')
+const {host, port, proxy} = require('../config.js')
 const common = require('./webpack.common')
+let devServer = {
+    host,
+    port,
+    proxy: {},
+    progress: true
+}
+for (let key in proxy) {
+    devServer.proxy[key] = proxy[key]
+}
 const webpackConfig = merge(common, {
     mode: 'development',
     devtool: 'source-map',
@@ -25,11 +34,7 @@ const webpackConfig = merge(common, {
             }
     	]
     },
-    devServer: {
-        host,
-        port,
-        progress: true
-    },
+    devServer,
     plugins: []
 })
 
